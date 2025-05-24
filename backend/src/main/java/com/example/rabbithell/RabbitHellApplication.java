e0471f227c279d4d88b4af027519d5d8e8dc38ed
giwon-wg <giwon8766@naver.com>
2025-05-24 18:05:07 +0900

    feat: 초기 개발 세팅 완료
    - Docker Compose 연동
    - MySQL/Redis 컨테이너 연결
    - .env 분리
    - CI 자동 빌드 구성

---
+++ backend/src/main/java/com/example/rabbithell/RabbitHellApplication.java
@@ -0,0 +1,27 @@
+package com.example.rabbithell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
+public class RabbitHellApplication {
+	public static void main(String[] args) {
+
+		// dotenv 환경변수 설정
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// 모든 항목 시스템 속성으로 설정
+		dotenv.entries().forEach(entry ->
+			System.setProperty(entry.getKey(), entry.getValue())
+		);
+
+		SpringApplication.run(RabbitHellApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/rabbithell/RabbitHellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbithell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitHellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
