e052d4d8e7d98c7676971f20d57b0f56cc3b6a17
Kibeom Park <97174348+Manhye@users.noreply.github.com>
2025-05-28 17:00:51 +0900

    package 정리 (#11)

---
+++ backend/src/main/java/com/example/rabbithell/domain/auth/dto/request/LoginRequest.java
@@ -0,0 +1,6 @@
+package com.example.rabbithell.domain.auth.dto.request;
+
+public record LoginRequest(
+    String email,
+    String password
+) {}
---
+++ backend/src/main/java/com/example/rabbithell/infrastructure/security/jwt/JwtProperties.java
@@ -0,0 +1,37 @@
+package com.example.rabbithell.infrastructure.security.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@ConfigurationProperties(prefix = "spring.jwt")
+@Getter
+@Setter
+public class JwtProperties {
+    private String secret;
+    private Token token;
+
+    @Getter
+    @Setter
+    public static class Token {
+        private String header;
+        private String prefix;
+        private Access access;
+        private Refresh refresh;
+
+        @Getter
+        @Setter
+        public static class Access {
+            private long minute;
+        }
+
+        @Getter
+        @Setter
+        public static class Refresh {
+            private long minute;
+        }
+    }
+}
