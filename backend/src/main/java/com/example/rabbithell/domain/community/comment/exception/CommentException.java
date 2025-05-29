a90ca1bef789adbff30c4a302bd69a62647e303d
기원 <gi.won.git@gmail.com>
2025-05-29 19:10:29 +0900

    Feat/exception (#38)
    * fix: Q 자동생성 충돌 문제 해결
    
    * feat/ Comment C 작성
    
    * feat/comment service 구현
    
    * feat/Comment 작성까지 완전 구현
    
    * feat: 댓글 CRUD 작성 완료 및 스웨거 테스트 통과
    
    * refactor: 컬럼명 변경 commentId -> id
    
    * refactor: 커스텀 예외 처리 일괄 적용
    
    * refactor: static 으로 변경
    
    * refactor: static 으로 변경
    
    * refactor: 어법 수정
    
    ---------
    
    Co-authored-by: giwon-wg <giwon8766@naver.com>

---
+++ backend/src/main/java/com/example/rabbithell/domain/community/comment/exception/CommentException.java
@@ -0,0 +1,23 @@
+package com.example.rabbithell.domain.community.comment.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.example.rabbithell.common.exception.BaseException;
+import com.example.rabbithell.domain.community.comment.exception.code.CommentExceptionCode;
+
+import lombok.Getter;
+
+@Getter
+public class CommentException extends BaseException {
+
+    private final CommentExceptionCode errorCode;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public CommentException(CommentExceptionCode errorCode) {
+        this.errorCode = errorCode;
+        this.httpStatus = errorCode.getStatus();
+        this.message = errorCode.getMessage();
+    }
+
+}
