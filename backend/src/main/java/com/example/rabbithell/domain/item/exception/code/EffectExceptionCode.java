eb45219f493b6dfde7a2a376e99edc1512362130
jiyun-im-dev <jiyun.im.dev@gmail.com>
2025-05-29 16:22:34 +0900

    feat: 특수 효과 커스텀 예외, 메서드 추가

---
+++ backend/src/main/java/com/example/rabbithell/domain/item/exception/code/EffectExceptionCode.java
@@ -0,0 +1,19 @@
+package com.example.rabbithell.domain.item.exception.code;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum EffectExceptionCode {
+
+    NO_SUCH_EFFECT(false, HttpStatus.NOT_FOUND, "효과가 존재하지 않습니다."),
+    ;
+
+    private final boolean success;
+    private final HttpStatus status;
+    private final String message;
+
+}
