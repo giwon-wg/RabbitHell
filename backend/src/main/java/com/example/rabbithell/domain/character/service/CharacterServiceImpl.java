fdb01ddae174127453072797cf7f702c6445d1c5
Kibeom Park <97174348+Manhye@users.noreply.github.com>
2025-05-29 10:33:00 +0900

    fix: 에러 수정 (#26)

--- backend/src/main/java/com/example/rabbithell/domain/character/service/CharacterServiceImpl.java
+++ backend/src/main/java/com/example/rabbithell/domain/character/service/CharacterServiceImpl.java
@@ -3,5 +3,5 @@ package com.example.rabbithell.domain.character.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CharaterServiceImpl {
+public class CharacterServiceImpl {
 }
