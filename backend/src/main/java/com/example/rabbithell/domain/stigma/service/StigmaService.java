f90f7acc37477f002fb92a1b09f38aed81a3fbca
prem3dev <ecstasy1508@gmail.com>
2025-05-29 15:54:42 +0900

    feat: Stigma 전체 조회, 개별 조회, 수정 서비스 인터페이스 로직 구현

--- backend/src/main/java/com/example/rabbithell/domain/stigma/service/StigmaService.java
+++ backend/src/main/java/com/example/rabbithell/domain/stigma/service/StigmaService.java
@@ -1,8 +1,18 @@
 package com.example.rabbithell.domain.stigma.service;
 
+import java.util.List;
+
 import com.example.rabbithell.domain.stigma.dto.request.CreateStigmaRequest;
-import com.example.rabbithell.domain.stigma.dto.response.CreateStigamResponse;
+import com.example.rabbithell.domain.stigma.dto.request.UpdateStigmaRequest;
+import com.example.rabbithell.domain.stigma.dto.response.StigamResponse;
 
 public interface StigmaService {
-    CreateStigamResponse create(CreateStigmaRequest request);
+
+    StigamResponse create(CreateStigmaRequest request);
+
+    List<StigamResponse> findAll(int pageNumber, int size);
+
+    StigamResponse findById(Long stigmaId);
+
+    void update(Long stigmaId, UpdateStigmaRequest request);
 }
