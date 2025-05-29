ca4ae11a0cde9cb0f33387e21a63658d0c318068
kibeo <kibeom0806@gmail.com>
2025-05-29 20:37:49 +0900

    feat: 전투 가능 필드 조회

--- backend/src/main/java/com/example/rabbithell/domain/battle/controller/BattleController.java
+++ backend/src/main/java/com/example/rabbithell/domain/battle/controller/BattleController.java
@@ -1,4 +1,40 @@
 package com.example.rabbithell.domain.battle.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.rabbithell.common.response.CommonResponse;
+import com.example.rabbithell.domain.auth.domain.AuthUser;
+import com.example.rabbithell.domain.battle.dto.request.GetBattleFieldsRequest;
+import com.example.rabbithell.domain.battle.dto.response.GetBattleFieldsResponse;
+import com.example.rabbithell.domain.battle.service.BattleService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/Battle")
+@RequiredArgsConstructor
 public class BattleController {
+
+    private final BattleService battleService;
+
+    @GetMapping
+    public ResponseEntity<CommonResponse<GetBattleFieldsResponse>> getAvailableMaps(
+        @AuthenticationPrincipal AuthUser authUser,
+        @Valid @RequestBody GetBattleFieldsRequest request
+    ){
+        return ResponseEntity.ok(CommonResponse.of(
+            true,
+            HttpStatus.OK.value(),
+            "전투 필드 조회 완료",
+            battleService.getBattleFields(authUser, request.characterId())
+        ));
+    }
 }
--- backend/src/main/java/com/example/rabbithell/domain/battle/service/BattleService.java
+++ backend/src/main/java/com/example/rabbithell/domain/battle/service/BattleService.java
@@ -1,7 +1,47 @@
 package com.example.rabbithell.domain.battle.service;
 
+import java.util.Objects;
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
+import com.example.rabbithell.domain.auth.domain.AuthUser;
+import com.example.rabbithell.domain.battle.dto.response.GetBattleFieldsResponse;
+import com.example.rabbithell.domain.battle.type.BattleFieldType;
+import com.example.rabbithell.domain.character.entity.Character;
+import com.example.rabbithell.domain.character.repository.CharacterRepository;
+import com.example.rabbithell.domain.village.exception.VillageException;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class BattleService {
+
+    BattleService battleService;
+    CharacterRepository characterRepository;
+
+
+
+    public GetBattleFieldsResponse getBattleFields(AuthUser authUser, Long characterId) {
+
+        Character character = verifyCharacter(authUser, characterId);
+
+        Set<BattleFieldType> unlockedRareMaps = character.getUnlockedRareMaps();
+
+        return new GetBattleFieldsResponse(unlockedRareMaps);
+    }
+
+
+    private Character verifyCharacter(AuthUser authUser, Long characterId) {
+
+        Character character = characterRepository.findByIdOrElseThrow(characterId);
+
+        if (!Objects.equals(character.getUser().getId(), authUser.getUserId())) {
+            throw new VillageException(CHARACTER_FORBIDDEN);
+        }
+
+        return character;
+    }
 }
+
