285df7d7337878a8e3547a6fe0410f0903c55abc
Kibeom Park <97174348+Manhye@users.noreply.github.com>
2025-05-29 15:55:15 +0900

    Feat/village (#32)
    * fix: 에러 수정
    
    * feat: 종족, 왕국 repository 추가
    
    * feat: 마을 이동 기능
    
    * feat: 은행 입금
    
    * feat: 은행 출금
    
    * feat: 치료 기능 구현
    
    * fix: 일부 오타 수정
    
    * fix: 단수화 및 오타 수정

--- backend/src/main/java/com/example/rabbithell/domain/monster/entity/MonsterEncounter.java
+++ backend/src/main/java/com/example/rabbithell/domain/monster/entity/MonsterEncounter.java
@@ -19,7 +19,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 @Entity
-@Table(name = "monster_encounter")
 @NoArgsConstructor
 @AllArgsConstructor
 public class MonsterEncounter {
--- backend/src/main/java/com/example/rabbithell/domain/specie/entity/Specie.java
+++ backend/src/main/java/com/example/rabbithell/domain/specie/entity/Specie.java
@@ -14,7 +14,6 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 @Entity
-@Table(name = "species")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Specie {
