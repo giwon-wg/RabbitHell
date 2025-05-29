65716a95e4d324f566fec2dfcb4210ae8421bc94
kibeo <kibeom0806@gmail.com>
2025-05-29 22:13:12 +0900

    fix: CI 재재도전

--- backend/src/test/java/com/example/rabbithell/RabbitHellApplicationTests.java
+++ backend/src/test/java/com/example/rabbithell/RabbitHellApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.rabbithell;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootTest
 class RabbitHellApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeAll
+    static void setup() {
+        Dotenv dotenv = Dotenv.configure().directory("backend").ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
