02a4dbf3c0aed6eae4f47ac6f88a0eae96472224
prem3dev <ecstasy1508@gmail.com>
2025-05-29 16:14:00 +0900

    feat: Stigma 삭제 api 생성

--- backend/src/main/java/com/example/rabbithell/domain/stigma/controller/StigmaController.java
+++ backend/src/main/java/com/example/rabbithell/domain/stigma/controller/StigmaController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -78,6 +79,18 @@ public class StigmaController {
 			CommonResponse.of(true,
 				HttpStatus.OK.value(),
 				"스티그마 수정 성공"));
+	}
+
+	@PreAuthorize("hasRole('ADMIN')")
+	@DeleteMapping("/{stigmaId}")
+	public ResponseEntity<CommonResponse<Void>> delete(
+		@PathVariable Long stigmaId
+	) {
+		stigmaService.delete(stigmaId);
+		return ResponseEntity.ok(
+			CommonResponse.of(true,
+				HttpStatus.OK.value(),
+				"스티그마 삭제 성공"));
 	}
 
 }
