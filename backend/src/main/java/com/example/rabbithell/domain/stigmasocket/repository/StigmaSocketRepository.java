ecf43b4dde0681b7c87cd96919a9c8645ea90eb5
prem3dev <ecstasy1508@gmail.com>
2025-05-30 11:35:44 +0900

    refactor: StigmaSocket Entity명 오타 수정에 따른 변경

--- backend/src/main/java/com/example/rabbithell/domain/stigmasocket/repository/StigmaSocketRepository.java
+++ backend/src/main/java/com/example/rabbithell/domain/stigmasocket/repository/StigmaSocketRepository.java
@@ -2,7 +2,7 @@ package com.example.rabbithell.domain.stigmasocket.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.rabbithell.domain.stigmasocket.entity.StigmaSoket;
+import com.example.rabbithell.domain.stigmasocket.entity.StigmaSocket;
 
-public interface StigmaSocketRepository extends JpaRepository<StigmaSoket, Long> {
+public interface StigmaSocketRepository extends JpaRepository<StigmaSocket, Long> {
 }
