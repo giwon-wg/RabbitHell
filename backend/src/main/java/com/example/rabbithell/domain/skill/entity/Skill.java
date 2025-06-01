464c93dec8e47a86e64222e7244257bb5d319f90
isom <smallsol.fa@gmail.com>
2025-06-02 02:11:46 +0900

    feat : Getter 추가

--- backend/src/main/java/com/example/rabbithell/domain/skill/entity/Skill.java
+++ backend/src/main/java/com/example/rabbithell/domain/skill/entity/Skill.java
@@ -2,22 +2,19 @@ package com.example.rabbithell.domain.skill.entity;
 
 import com.example.rabbithell.domain.job.entity.Job;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@Getter
 public class Skill {
 
     @Id
