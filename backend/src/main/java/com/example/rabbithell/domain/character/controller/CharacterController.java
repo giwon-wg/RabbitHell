e5b024c226c04d384e669d700db7a8381ee4f62f
isom <smallsol.fa@gmail.com>
2025-06-02 02:14:31 +0900

    refactor : RequestBody swagger 변경

--- backend/src/main/java/com/example/rabbithell/domain/character/controller/CharacterController.java
+++ backend/src/main/java/com/example/rabbithell/domain/character/controller/CharacterController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +22,7 @@ import com.example.rabbithell.domain.character.dto.response.CharacterInfoRespons
 import com.example.rabbithell.domain.character.dto.response.CharacterPersonalInfoResponse;
 import com.example.rabbithell.domain.character.service.CharacterService;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
