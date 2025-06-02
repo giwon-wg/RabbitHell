ce85ab7df25fb46961113f9520ac390f36c5c09e
x0x22 <smallsol.fa@gmail.com>
2025-06-02 12:01:07 +0900

    Feat/skill (#75)
    * fix : 사용하지 않는 repository 삭제
    
    * feat : 최대 스탯 추가

--- backend/src/main/java/com/example/rabbithell/domain/job/repository/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rabbithell.domain.job.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.rabbithell.domain.job.entity.Job;
-
-public interface JobRepository extends JpaRepository<Job, Long> {
-}
