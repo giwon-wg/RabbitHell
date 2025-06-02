5f33554a0e871663509e0e5ca6b5cab58935f7f6
kibeo <kibeom0806@gmail.com>
2025-06-02 09:23:38 +0900

    fix: 안 쓰는 에러코드 삭제

--- backend/src/main/java/com/example/rabbithell/domain/village/exception/code/VillageExceptionCode.java
+++ backend/src/main/java/com/example/rabbithell/domain/village/exception/code/VillageExceptionCode.java
@@ -8,13 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum VillageExceptionCode {
-    VILLAGE_NOT_CONNECTED(false, HttpStatus.BAD_REQUEST, "인접하지 않은 마을로는 이동할 수 없습니다."),
-    BELOW_ZERO(false, HttpStatus.BAD_REQUEST, "음수 값이 입력되었습니다."),
-    NOT_ENOUGH_MONEY(false, HttpStatus.BAD_REQUEST, "돈이 부족합니다."),
-    VILLAGE_NOT_FOUND(false, HttpStatus.NOT_FOUND, "존재하지 않는 마을입니다.")
-    ;
+	VILLAGE_NOT_CONNECTED(false, HttpStatus.BAD_REQUEST, "인접하지 않은 마을로는 이동할 수 없습니다."),
+	VILLAGE_NOT_FOUND(false, HttpStatus.NOT_FOUND, "존재하지 않는 마을입니다.");
 
-    private final boolean success;
-    private final HttpStatus status;
-    private final String message;
+	private final boolean success;
+	private final HttpStatus status;
+	private final String message;
 }
