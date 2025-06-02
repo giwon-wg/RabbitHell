af08f689f00cd219f56254207880aa9d32d087b9
jiyun-im-dev <133330951+jiyun-im-dev@users.noreply.github.com>
2025-06-02 10:09:31 +0900

    Feat/inventory item: 인벤토리 아이템 조회, 장착 기능 추가 (#68)
    * feat: 상점 조회 기능 추가 #25
    
    * style: 메서드 이름 수정
    
    * feat: 상점 생성 기능 추가(어드민) #24
    
    * refactor: 리포지토리에 findByIdOrElseThrow() 메서드 추가
    
    * feat: 상점 수정 기능 추가(어드민)
    
    * feat: 상점 삭제 기능 추가(어드민) #24
    
    * style: 엔터 수정
    
    * refactor: 메서드명 수정(오타)
    
    * fix: 누락된 @Valid 애노테이션 추가
    
    * refactor: 리포지토리의 findByIdOrElseThrow() 메서드로 변경
    
    * feat: Effect 엔티티 컬럼 추가
    
    * feat: Item 엔티티 컬럼 추가
    
    * feat: Item 커스텀 예외 추가
    
    * feat: InventoryItem이 BaseEntity 상속 및 하드 딜리트로 변경
    
    * feat: 아이템 특수 효과 타입 enum 추가
    
    * style: remove unused imports
    
    * feat: 아이템 특수 효과 엔티티 추가
    
    * feat: 특수 효과 커스텀 예외, 메서드 추가
    
    * feat: 특수 효과 생성 API 추가
    
    * style: 줄바꿈 수정
    
    * feat: 특수 효과 수정 API 추가
    
    * feat: 특수 효과 단건 조회 API 추가
    
    * feat: 특수 효과 전체 조회 API 추가
    
    * feat: 특수 효과 삭제 API 추가
    
    * style: 코드 형식 수정
    
    * feat: 접근 권한 어드민으로 제한
    
    * style: 메서드 순서 변경
    
    * feat: 아이템 생성 API 추가
    
    * style: 줄바꿈 수정
    
    * feat: Item에 ItemType 컬럼 추가
    
    * feat: 아이템 전체 조회 API 추가
    
    * refactor: 메서드명 변경(find->get)
    
    * feat: 아이템 단건 조회 API 추가
    
    * feat: 아이템 수정 API 추가
    
    * feat: 아이템 삭제 API 추가
    
    * style: 컨트롤러 이름 변경
    
    * style: remove unused imports
    
    * feat: 가입 시 인벤토리 생성 로직 추가
    
    * feat: slot 종류 축소
    
    * feat: 인벤토리 확장 API 추가
    
    * feat: 인벤토리 아이템 전체 조회 API 추가
    
    * style: 들여쓰기를 탭으로 변경
    
    * fix: 메서드명 오타 수정
    
    * refactor: 커스텀 예외 코드명 변경
    
    * fix: 상점 null 값 체크 추가
    
    * feat: 인벤토리 아이템 단건 조회 API 추가
    
    * fix: @Transactional 추가
    
    * fix: Inventory 엔티티 수정
    
    * feat: ItemType 추가
    
    * feat: 아이템 컬럼 변경
    
    * feat: 아이템 장착 기능 추가

---
+++ backend/src/main/java/com/example/rabbithell/domain/inventory/dto/response/EquippedItem.java
@@ -0,0 +1,20 @@
+package com.example.rabbithell.domain.inventory.dto.response;
+
+import com.example.rabbithell.domain.inventory.entity.InventoryItem;
+import com.example.rabbithell.domain.inventory.enums.Slot;
+
+public record EquippedItem(
+	Long itemId,
+	Long characterId,
+	Slot slot,
+	Integer durability
+) {
+	public static EquippedItem fromEntity(InventoryItem inventoryItem) {
+		return new EquippedItem(
+			inventoryItem.getItem().getId(),
+			inventoryItem.getCharacter().getId(),
+			inventoryItem.getSlot(),
+			inventoryItem.getDurability()
+		);
+	}
+}
