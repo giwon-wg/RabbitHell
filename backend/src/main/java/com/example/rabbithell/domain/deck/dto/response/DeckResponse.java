0ede1f00d6c99723d5a0aa4488ebef7830b4ca1a
prem3dev <ecstasy1508@gmail.com>
2025-06-04 21:31:11 +0900

    feat: DeckResponse 생성 메서드 수정

--- backend/src/main/java/com/example/rabbithell/domain/deck/dto/response/DeckResponse.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/dto/response/DeckResponse.java
@@ -3,10 +3,8 @@ package com.example.rabbithell.domain.deck.dto.response;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import com.example.rabbithell.domain.clover.entity.Clover;
 import com.example.rabbithell.domain.deck.entity.Deck;
 import com.example.rabbithell.domain.deck.enums.PawCardSlot;
-import com.example.rabbithell.domain.pawcard.entity.PawCard;
 import com.example.rabbithell.domain.pawcard.enums.CardEmblem;
 
 public record DeckResponse(
@@ -21,18 +19,15 @@ public record DeckResponse(
 	LocalDateTime createdAt,
 	LocalDateTime modifiedAt
 ) {
-	public static DeckResponse fromEntity(
-		Deck deck,
-		Clover clover,
-		PawCard pawCard) {
+	public static DeckResponse fromEntity(Deck deck) {
 		return new DeckResponse(
 			deck.getId(),
-			clover.getId(),
-			pawCard.getId(),
-			pawCard.getCardNumber(),
-			pawCard.getCardEmblem(),
-			pawCard.getRatio(),
-			pawCard.getDescription(),
+			deck.getClover().getId(),
+			deck.getPawCard().getId(),
+			deck.getPawCard().getCardNumber(),
+			deck.getPawCard().getCardEmblem(),
+			deck.getPawCard().getRatio(),
+			deck.getPawCard().getDescription(),
 			deck.getPawCardSlot(),
 			deck.getCreatedAt(),
 			deck.getModifiedAt()
