730e7296a85cf7e5888d71207970ef1957e45f28
prem3dev <ecstasy1508@gmail.com>
2025-06-04 12:15:03 +0900

    feat: replace StigmaSocketService with DeckService

--- backend/src/main/java/com/example/rabbithell/domain/stigmasocket/service/StigmaSocketService.java
+++
@@ -1,8 +0,0 @@
-package com.example.rabbithell.domain.stigmasocket.service;
-
-import com.example.rabbithell.domain.stigmasocket.dto.request.CreateStigmaSocketRequest;
-import com.example.rabbithell.domain.stigmasocket.dto.response.StigmaSocketResponse;
-
-public interface StigmaSocketService {
-	StigmaSocketResponse createStigmaSocket(CreateStigmaSocketRequest request);
-}
