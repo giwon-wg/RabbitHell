14f9f66de7b3cfbd3b8e2676571ea60bad1caac3
prem3dev <ecstasy1508@gmail.com>
2025-06-04 21:23:50 +0900

    feat: DeckExceptionCode 내용 수정

--- backend/src/main/java/com/example/rabbithell/domain/deck/exception/code/DeckExceptionCode.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/exception/code/DeckExceptionCode.java
@@ -1,18 +1,17 @@
 package com.example.rabbithell.domain.deck.exception.code;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum DeckExceptionCode {
 
-    STIGMASOCKET_NOT_FOUND(false, HttpStatus.NOT_FOUND, "스티그마 소켓을 찾을 수 없습니다."),
-    ;
+	DECK_NOT_FOUND(false, HttpStatus.NOT_FOUND, "유저 클로버의 해당 덱을 찾을 수 없습니다.");
 
-    private final boolean success;
-    private final HttpStatus status;
-    private final String message;
+	private final boolean success;
+	private final HttpStatus status;
+	private final String message;
 }
