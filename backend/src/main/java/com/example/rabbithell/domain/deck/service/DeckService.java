a08c4b1700837cadfd014aa33ef78e89cee534f5
prem3dev <ecstasy1508@gmail.com>
2025-06-04 21:32:05 +0900

    feat: DeckService 생성
    - 덱 전체 동적 페이지 조회 인터페이스 메서드 구현
    - 덱 개별 조회 인터페이스 메서드 구현

---
+++ backend/src/main/java/com/example/rabbithell/domain/deck/service/DeckService.java
@@ -0,0 +1,11 @@
+package com.example.rabbithell.domain.deck.service;
+
+import com.example.rabbithell.common.dto.response.PageResponse;
+import com.example.rabbithell.domain.deck.dto.request.DeckCond;
+import com.example.rabbithell.domain.deck.dto.response.DeckResponse;
+
+public interface DeckService {
+	PageResponse<DeckResponse> findAllDeckByCond(Long cloverId, int pageNumber, int size, DeckCond cond);
+
+	DeckResponse findDeckById(Long deckId, Long cloverId);
+}
