d6478153ed6cab6e4a89fe276318a09e35da1ab8
prem3dev <ecstasy1508@gmail.com>
2025-06-04 21:16:54 +0900

    feat: cloverId index로 추가, 복합 유니크 추가
    - 하나의 clover 안에 slot은 유니크
    - 하나의 clover 안에 pawCard는 유니크

--- backend/src/main/java/com/example/rabbithell/domain/deck/entity/Deck.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/entity/Deck.java
@@ -11,9 +11,11 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,7 +26,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @Getter
 @Entity
-@Table(name = "deck")
+@Table(name = "deck",
+	indexes = {
+		@Index(name = "idx_clover_id", columnList = "clover_id")
+	},
+	uniqueConstraints = {
+		@UniqueConstraint(columnNames = {"clover_id", "paw_card_slot"}),
+		@UniqueConstraint(columnNames = {"clover_id", "paw_card_id"})
+	})
 public class Deck extends BaseEntity {
 
 	@Id
@@ -39,7 +48,7 @@ public class Deck extends BaseEntity {
 	@JoinColumn(name = "clover_id", nullable = false)
 	private Clover clover;
 
-	@Column(name = "paw_card_slot", unique = true, nullable = true)
+	@Column(name = "paw_card_slot", nullable = true)
 	private PawCardSlot pawCardSlot;
 
 	@Builder
