0df8b6419871c97b2752feeb0d96feb32a5537b2
prem3dev <ecstasy1508@gmail.com>
2025-06-04 21:30:44 +0900

    feat: DeckRepository에 DeckQueryRepository 연결

--- backend/src/main/java/com/example/rabbithell/domain/deck/repository/DeckRepository.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/repository/DeckRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rabbithell.domain.deck.entity.Deck;
 
-public interface DeckRepository extends JpaRepository<Deck, Long> {
+public interface DeckRepository extends JpaRepository<Deck, Long>, DeckQueryRepository {
 }
