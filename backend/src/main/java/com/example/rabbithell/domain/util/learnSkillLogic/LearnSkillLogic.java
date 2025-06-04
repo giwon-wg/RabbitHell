049d08d137015351f0086d25f5f382d1d4ace86d
isom <smallsol.fa@gmail.com>
2025-06-05 00:55:32 +0900

    feat : 커스텀 예외 처리

--- backend/src/main/java/com/example/rabbithell/domain/util/learnSkillLogic/LearnSkillLogic.java
+++ backend/src/main/java/com/example/rabbithell/domain/util/learnSkillLogic/LearnSkillLogic.java
@@ -1,11 +1,14 @@
 package com.example.rabbithell.domain.util.learnSkillLogic;
 
+import static com.example.rabbithell.domain.skill.exception.code.SkillExceptionCode.*;
+
 import com.example.rabbithell.domain.character.entity.GameCharacter;
 import com.example.rabbithell.domain.character.repository.CharacterRepository;
 import com.example.rabbithell.domain.characterSkill.entity.CharacterSkill;
 import com.example.rabbithell.domain.characterSkill.repository.CharacterSkillRepository;
 import com.example.rabbithell.domain.job.entity.Job;
 import com.example.rabbithell.domain.skill.entity.Skill;
+import com.example.rabbithell.domain.skill.exception.SkillException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,17 +25,17 @@ public class LearnSkillLogic {
 
 		// 직업 카테고리 확인
 		if (characterJob.getJobCategory() != skillRequiredJob.getJobCategory()) {
-			throw new IllegalArgumentException("직업 계열이 달라 이 스킬을 배울 수 없습니다.");
+			throw new SkillException(INVALID_JOB_FOR_SKILL);
 		}
 
 		// 티어는 같거나 높아야 함
 		if (!characterJob.getTier().isSameOrHigherThan(skillRequiredJob.getTier())) {
-			throw new IllegalArgumentException("현재 직업 티어로는 해당 스킬을 배울 수 없습니다.");
+			throw new SkillException(INSUFFICIENT_TIER_FOR_SKILL);
 		}
 
 		// 이미 배운 스킬은 배울 수 없음.
 		if (characterSkillRepository.existsByCharacterAndSkill(character, skill)) {
-			throw new IllegalArgumentException("이미 배운 스킬입니다.");
+			throw new SkillException(ALREADY_LEARNED);
 		}
 
 
