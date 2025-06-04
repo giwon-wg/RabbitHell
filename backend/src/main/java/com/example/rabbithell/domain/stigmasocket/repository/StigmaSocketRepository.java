d0cce46731da594beff9bfaff55d681c273e7f82
prem3dev <ecstasy1508@gmail.com>
2025-06-04 12:13:55 +0900

    feat: replace StigmaSocketRepository with DeckRepository

--- backend/src/main/java/com/example/rabbithell/domain/stigmasocket/repository/StigmaSocketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rabbithell.domain.stigmasocket.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.rabbithell.domain.stigmasocket.entity.StigmaSocket;
-
-public interface StigmaSocketRepository extends JpaRepository<StigmaSocket, Long> {
-}
