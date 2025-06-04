856082a627a0e0e5f2e240819282308ee5d06403
prem3dev <ecstasy1508@gmail.com>
2025-06-04 11:52:04 +0900

    feat: AdminDeckServiceImpl 생성
    - 관리자 임의 deck 생성 서비스 로직 구현

---
+++ backend/src/main/java/com/example/rabbithell/domain/deck/service/AdminDeckServiceImpl.java
@@ -0,0 +1,35 @@
+package com.example.rabbithell.domain.deck.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.rabbithell.domain.clover.entity.Clover;
+import com.example.rabbithell.domain.clover.repository.CloverRepository;
+import com.example.rabbithell.domain.deck.dto.request.CreateDeckRequest;
+import com.example.rabbithell.domain.deck.dto.response.DeckResponse;
+import com.example.rabbithell.domain.deck.entity.Deck;
+import com.example.rabbithell.domain.deck.repository.DeckRepository;
+import com.example.rabbithell.domain.pawcard.entity.PawCard;
+import com.example.rabbithell.domain.pawcard.repository.PawCardRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AdminDeckServiceImpl implements AdminDeckService {
+
+	private final DeckRepository deckRepository;
+	private final PawCardRepository pawCardRepository;
+	private final CloverRepository cloverRepository;
+
+	@Override
+	public DeckResponse createDeck(Long cloverId, CreateDeckRequest request) {
+		Clover clover = cloverRepository.findByIdOrElseThrow(cloverId);
+		PawCard pawCard = pawCardRepository.findByIdOrElseThrow(request.pawCardId());
+		Deck deck = Deck.builder()
+			.clover(clover)
+			.pawCard(pawCard)
+			.build();
+		deckRepository.save(deck);
+		return DeckResponse.fromEntity(deck, clover, pawCard);
+	}
+}
