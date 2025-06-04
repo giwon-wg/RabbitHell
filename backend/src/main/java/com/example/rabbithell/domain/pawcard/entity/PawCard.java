241ec1249e1ab8df3ea9d3bf5987c0bd667aa72f
prem3dev <ecstasy1508@gmail.com>
2025-06-04 09:36:59 +0900

    feat: PawCard Entity 생성

---
+++ backend/src/main/java/com/example/rabbithell/domain/pawcard/entity/PawCard.java
@@ -0,0 +1,116 @@
+package com.example.rabbithell.domain.pawcard.entity;
+
+import java.math.BigDecimal;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import com.example.rabbithell.common.audit.BaseEntity;
+import com.example.rabbithell.common.effect.enums.DomainType;
+import com.example.rabbithell.common.effect.enums.StatCategory;
+import com.example.rabbithell.common.effect.enums.StatType;
+import com.example.rabbithell.domain.pawcard.enums.CardEmblem;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@DynamicUpdate
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "paw_card")
+public class PawCard extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(precision = 5, scale = 4, nullable = false)
+	private BigDecimal ratio;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(name = "is_deleted", nullable = false)
+	private Boolean isDeleted;
+
+	@Column(name = "card_number", nullable = false)
+	private Integer cardNumber;
+
+	@Enumerated
+	@Column(name = "card_emblem", nullable = false)
+	private CardEmblem cardEmblem;
+
+	@Enumerated
+	@Column(name = "Stat_type", nullable = false)
+	private StatType statType;
+
+	@Enumerated
+	@Column(name = "stat_category", nullable = false)
+	private StatCategory statCategory;
+
+	@Enumerated
+	@Column(name = "domain_type", nullable = false)
+	private DomainType domainType;
+
+	@Builder
+	public PawCard(
+		BigDecimal ratio,
+		String description,
+		Integer cardNumber,
+		CardEmblem cardEmblem,
+		StatType statType,
+		StatCategory statCategory,
+		DomainType domainType,
+		Boolean isDelete
+
+	) {
+		this.ratio = ratio;
+		this.description = description;
+		this.cardNumber = cardNumber;
+		this.cardEmblem = cardEmblem;
+		this.statType = statType;
+		this.statCategory = statCategory;
+		this.domainType = domainType;
+		this.isDeleted = isDelete;
+	}
+
+	public void markAsDeleted() {
+		this.isDeleted = true;
+	}
+
+	public void changeRatio(BigDecimal ratio) {
+		this.ratio = ratio;
+	}
+
+	public void changeDescription(String description) {
+		this.description = description;
+	}
+
+	public void changeCardNumber(Integer cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+
+	public void changeCardEmblem(CardEmblem cardEmblem) {
+		this.cardEmblem = cardEmblem;
+	}
+
+	public void changeStatType(StatType statType) {
+		this.statType = statType;
+	}
+
+	public void changeStatCategory(StatCategory statCategory) {
+		this.statCategory = statCategory;
+	}
+
+	public void changeDomainType(DomainType domainType) {
+		this.domainType = domainType;
+	}
+}
