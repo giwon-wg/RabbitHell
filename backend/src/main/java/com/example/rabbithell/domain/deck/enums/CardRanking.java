b6109d9f9ecaf048082fe1ec3d1743a86cf73634
prem3dev <ecstasy1508@gmail.com>
2025-06-04 09:33:46 +0900

    feat: CardRanking 추가

---
+++ backend/src/main/java/com/example/rabbithell/domain/deck/enums/CardRanking.java
@@ -0,0 +1,7 @@
+package com.example.rabbithell.domain.deck.enums;
+
+public enum CardRanking {
+	HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND,
+	FOUR_OF_A_KIND, STRAIGHT, FLUSH,
+	STRAIGHT_FLUSH, ROYAL_STRAIGHT_FLUSH;
+}
