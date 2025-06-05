539305b34882475c3957d914f62130e2523542fd
giwon-wg <giwon8766@naver.com>
2025-06-05 17:49:42 +0900

    feat: 프론트 마을, 월드맵 구현

--- backend/src/main/java/com/example/rabbithell/domain/village/repository/VillageConnectionRepository.java
+++ backend/src/main/java/com/example/rabbithell/domain/village/repository/VillageConnectionRepository.java
@@ -1,8 +1,12 @@
 package com.example.rabbithell.domain.village.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rabbithell.domain.village.entity.VillageConnection;
 
 public interface VillageConnectionRepository extends JpaRepository<VillageConnection, Long> {
+
+	List<VillageConnection> findByFromVillage(Long fromVillageId);
 }
