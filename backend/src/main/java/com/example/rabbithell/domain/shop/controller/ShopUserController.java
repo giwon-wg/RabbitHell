bb3011e05ec9c29fdba88a8f9f58d4ff97d3a4ac
jiyun-im-dev <133330951+jiyun-im-dev@users.noreply.github.com>
2025-06-05 16:31:27 +0900

    Feat/shop: 아이템 조회, 구매, 판매 기능 추가 (#96)
    * style: remove unused imports
    
    * fix: 장착 가능한 아이템 리스트에 다른 캐릭터가 장착한 아이템은 안 보이도록 수정
    
    * feat: 아이템 응답에 필드 추가
    
    * feat: 아이템 장착 시 동일한 슬롯에 먼저 장착된 아이템은 장착 해제되도록 수정
    
    * feat: 아이템 종류에 따라 장착 부위가 정해지도록 기능 수정
    
    * refactor: 위력 필드를 인벤토리 아이템으로 이동
    
    * style: remove unused imports
    
    * feat: 상점 아이템 상세 조회 API 추가
    
    * feat: 상점 아이템 전체 조회, 상점에 아이템 등록 API 추가
    
    * fix: power 필드를 InventoryItem으로 이동
    
    * feat: updateShop() 메서드 추가
    
    * feat: 상점에서 아이템 구매 API 추가
    
    * feat: 상점에서 아이템 판매 API 추가
    
    * feat: 캐릭터 별 장착 인벤토리 아이템 조회 로직 추가

--- backend/src/main/java/com/example/rabbithell/domain/shop/controller/ShopUserController.java
+++ backend/src/main/java/com/example/rabbithell/domain/shop/controller/ShopUserController.java
@@ -1,13 +1,27 @@
 package com.example.rabbithell.domain.shop.controller;
 
+import java.security.Principal;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.rabbithell.common.dto.response.PageResponse;
 import com.example.rabbithell.common.response.CommonResponse;
+import com.example.rabbithell.domain.auth.domain.AuthUser;
+import com.example.rabbithell.domain.shop.dto.response.BuyItemResponse;
+import com.example.rabbithell.domain.shop.dto.response.SellItemResponse;
+import com.example.rabbithell.domain.shop.dto.response.ShopItemResponse;
 import com.example.rabbithell.domain.shop.dto.response.ShopResponse;
 import com.example.rabbithell.domain.shop.service.ShopService;
 
@@ -27,6 +41,56 @@ public class ShopUserController {
 			HttpStatus.OK.value(),
 			"상점 조회 성공",
 			shopService.getShopById(shopId)
+		));
+	}
+
+	@GetMapping("/{shopId}/items/{itemId}")
+	public ResponseEntity<CommonResponse<ShopItemResponse>> getShopItem(@PathVariable Long itemId) {
+		return ResponseEntity.ok(CommonResponse.of(
+			true,
+			HttpStatus.OK.value(),
+			"상점 아이템 상세 조회 성공",
+			shopService.getShopItem(itemId)
+		));
+	}
+
+	@GetMapping("/{shopId}/items")
+	public ResponseEntity<CommonResponse<PageResponse<ShopItemResponse>>> getAllShopItems(
+		@PathVariable Long shopId,
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size
+	) {
+		Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
+
+		return ResponseEntity.ok(CommonResponse.of(
+			true,
+			HttpStatus.OK.value(),
+			"상점 아이템 전체 조회 성공",
+			shopService.getAllShopItems(shopId, pageable)
+		));
+	}
+
+	@PostMapping("/{shopId}/items/{itemId}/buy")
+	public ResponseEntity<CommonResponse<BuyItemResponse>> buyItem(
+		@AuthenticationPrincipal AuthUser authUser,
+		@PathVariable Long itemId,
+		@RequestParam int quantity,
+		Principal principal, UserDetails authenticatedPrincipal) {
+		return ResponseEntity.ok(CommonResponse.of(
+			true,
+			HttpStatus.OK.value(),
+			"상점 아이템 구매 성공",
+			shopService.buyItem(authUser.getUserId(), itemId, quantity)
+		));
+	}
+
+	@PostMapping("/{shopId}/sell")
+	public ResponseEntity<CommonResponse<SellItemResponse>> sellItem(@RequestParam Long inventoryItemId) {
+		return ResponseEntity.ok(CommonResponse.of(
+			true,
+			HttpStatus.OK.value(),
+			"아이템 판매 성공",
+			shopService.sellItem(inventoryItemId)
 		));
 	}
 
