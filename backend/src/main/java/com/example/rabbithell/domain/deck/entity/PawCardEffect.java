2a966ff623948fbf89ac2768679eeb0c574fc56d
prem3dev <ecstasy1508@gmail.com>
2025-06-09 09:18:10 +0900

    feat: PawCardEffect Entity 생성

---
+++ backend/src/main/java/com/example/rabbithell/domain/deck/entity/PawCardEffect.java
@@ -0,0 +1,61 @@
+package com.example.rabbithell.domain.deck.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.rabbithell.domain.clover.entity.Clover;
+import com.example.rabbithell.domain.deck.enums.CardRanking;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "paw_card_effect")
+public class PawCardEffect {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@Enumerated(EnumType.STRING)
+	private CardRanking cardRanking;
+
+	@ManyToOne
+	@JoinColumn(name = "clover_id", nullable = false)
+	Clover clover;
+
+	@OneToMany(mappedBy = "pawCardEffect", cascade = CascadeType.ALL)
+	private List<EffectDetail> details = new ArrayList<>();
+
+	@Builder
+	PawCardEffect(
+		CardRanking cardRanking,
+		Clover clover
+	) {
+		this.cardRanking = cardRanking;
+		this.clover = clover;
+	}
+
+	public void addEffectDetail(EffectDetail effectDetail) {
+		this.details.add(effectDetail);
+		effectDetail.assignToEffect(this);
+	}
+
+	public void markCardRanking(CardRanking cardRanking) {
+		this.cardRanking = cardRanking;
+	}
+}
