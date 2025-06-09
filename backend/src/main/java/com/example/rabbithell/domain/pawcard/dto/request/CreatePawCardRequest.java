1abbbb02ad62c9c6877f5d3b0f4ebdd6f5207805
prem3dev <ecstasy1508@gmail.com>
2025-06-09 09:04:07 +0900

    feat: BigDecimal 부분을 전부 Integer로 변경

--- backend/src/main/java/com/example/rabbithell/domain/pawcard/dto/request/CreatePawCardRequest.java
+++ backend/src/main/java/com/example/rabbithell/domain/pawcard/dto/request/CreatePawCardRequest.java
@@ -1,24 +1,16 @@
 package com.example.rabbithell.domain.pawcard.dto.request;
 
-import java.math.BigDecimal;
-
 import com.example.rabbithell.common.effect.enums.StatType;
 import com.example.rabbithell.domain.pawcard.enums.CardEmblem;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreatePawCardRequest(
 
-	@Schema(description = "0~1까지 소숫점 4자리수", example = "0.1234")
-	@DecimalMin("0.0000")
-	@DecimalMax("1.0000")
-	@Digits(integer = 1, fraction = 4)
-	@NotNull BigDecimal ratio,
+	@Schema(description = "x 퍼센트(x는 양의 정수)", example = "100")
+	@NotNull Integer ratioPercent,
 
 	@Schema(description = "설명", example = "클로버 5")
 	@NotBlank String description,
