9ed56b5ff9fa24813682a30e660e1b33d0840a6b
prem3dev <ecstasy1508@gmail.com>
2025-06-09 09:09:25 +0900

    feat: DckQueryRepository에 인터페이스 메서드들 추가

--- backend/src/main/java/com/example/rabbithell/domain/deck/repository/DeckQueryRepository.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/repository/DeckQueryRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 
 import com.example.rabbithell.domain.deck.dto.request.DeckCond;
 import com.example.rabbithell.domain.deck.entity.Deck;
+import com.example.rabbithell.domain.deck.enums.PawCardSlot;
 
 public interface DeckQueryRepository {
 	List<Deck> findAllByCondition(Long cloverId, DeckCond cond, Pageable pageable);
@@ -13,4 +14,10 @@ public interface DeckQueryRepository {
 	long countByCondition(Long cloverId, DeckCond cond);
 
 	Deck findByIdAndCloverIdOrElseThrow(Long deckId, Long cloverId);
+
+	List<Deck> findLockedByCloverIdAndSlots(Long cloverId, List<PawCardSlot> pawCardSlots);
+
+	List<Deck> lockDecksByCloverIdAndIds(Long cloverId, List<Long> ids);
+
+	List<Deck> findEquippedByCloverIdWithLock(Long cloverId);
 }
