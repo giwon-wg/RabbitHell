edff571c3765ea898c150199dccdaaaa100b8d29
prem3dev <ecstasy1508@gmail.com>
2025-06-09 09:12:23 +0900

    feat: activePawCard 인터페이스 메서드 생성

--- backend/src/main/java/com/example/rabbithell/domain/deck/service/DeckService.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/service/DeckService.java
@@ -1,11 +1,15 @@
 package com.example.rabbithell.domain.deck.service;
 
 import com.example.rabbithell.common.dto.response.PageResponse;
+import com.example.rabbithell.domain.deck.dto.request.BatchActivePawCardRequest;
 import com.example.rabbithell.domain.deck.dto.request.DeckCond;
+import com.example.rabbithell.domain.deck.dto.response.BatchActivePawCardResponse;
 import com.example.rabbithell.domain.deck.dto.response.DeckResponse;
 
 public interface DeckService {
 	PageResponse<DeckResponse> findAllDeckByCond(Long cloverId, int pageNumber, int size, DeckCond cond);
 
 	DeckResponse findDeckById(Long deckId, Long cloverId);
+
+	BatchActivePawCardResponse activePawCard(Long cloverId, BatchActivePawCardRequest request);
 }
