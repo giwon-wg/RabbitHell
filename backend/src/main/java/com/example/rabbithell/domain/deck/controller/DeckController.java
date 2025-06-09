1805dab67ce872c92bed45da135afdb93f3a0560
prem3dev <ecstasy1508@gmail.com>
2025-06-09 09:06:48 +0900

    feat: ActivePawcard api 생성

--- backend/src/main/java/com/example/rabbithell/domain/deck/controller/DeckController.java
+++ backend/src/main/java/com/example/rabbithell/domain/deck/controller/DeckController.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.rabbithell.common.dto.response.PageResponse;
 import com.example.rabbithell.common.response.CommonResponse;
 import com.example.rabbithell.domain.auth.domain.AuthUser;
+import com.example.rabbithell.domain.deck.dto.request.BatchActivePawCardRequest;
 import com.example.rabbithell.domain.deck.dto.request.DeckCond;
+import com.example.rabbithell.domain.deck.dto.response.BatchActivePawCardResponse;
 import com.example.rabbithell.domain.deck.dto.response.DeckResponse;
 import com.example.rabbithell.domain.deck.service.DeckService;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 
@@ -52,5 +57,18 @@ public class DeckController {
 				HttpStatus.OK.value(),
 				"덱 개별 조회 성공",
 				deckService.findDeckById(deckId, authUser.getCloverId())));
+	}
+
+	@PostMapping()
+	public ResponseEntity<CommonResponse<BatchActivePawCardResponse>> activePawCard(
+		@AuthenticationPrincipal AuthUser authUser,
+		@RequestBody @Valid BatchActivePawCardRequest request
+	) {
+		return ResponseEntity.ok(
+			CommonResponse.of(true,
+				HttpStatus.OK.value(),
+				"PawCard다!! 뀨!!!",
+				deckService.activePawCard(authUser.getCloverId(), request))
+		);
 	}
 }
