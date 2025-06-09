6c870fb925e9673e7d68d0da5278c6981ea04776
kibeo <kibeom0806@gmail.com>
2025-06-09 19:01:21 +0900

    Feat: 졌을때와 이겼을 떄 구현

---
+++ backend/src/main/java/com/example/rabbithell/domain/battle/postProcess/strategy/DrawRewardStrategy.java
@@ -0,0 +1,52 @@
+package com.example.rabbithell.domain.battle.postProcess.strategy;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.rabbithell.domain.battle.type.BattleFieldType;
+import com.example.rabbithell.domain.battle.vo.BattleRewardResultVo;
+import com.example.rabbithell.domain.character.entity.GameCharacter;
+import com.example.rabbithell.domain.clover.entity.Clover;
+import com.example.rabbithell.domain.clover.repository.CloverRepository;
+import com.example.rabbithell.domain.monster.entity.Monster;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class DrawRewardStrategy implements BattleRewardStrategy {
+
+	private final CloverRepository cloverRepository;
+
+	@Override
+	@Transactional
+	public BattleRewardResultVo applyReward(Clover clover, List<GameCharacter> team, Monster monster,
+		BattleFieldType fieldType) {
+
+		Clover updatedClover = cloverRepository.findByIdOrElseThrow(clover.getId());
+		updatedClover.clearUnlockedRareMaps();
+
+		List<Integer> zero = Arrays.asList(0, 0, 0, 0);
+		List<List<Integer>> zeros = Arrays.asList(zero, zero, zero, zero);
+		List<Integer> levels = team.stream().map(GameCharacter::getLevel).toList();
+
+		cloverRepository.save(updatedClover);
+
+		return new BattleRewardResultVo(
+			0,
+			0,
+			0,
+			clover.getCash(),
+			zero,
+			zero,
+			levels,
+			zero,
+			zeros,
+			null,
+			updatedClover.getUnlockedRareMaps()
+		);
+	}
+}
