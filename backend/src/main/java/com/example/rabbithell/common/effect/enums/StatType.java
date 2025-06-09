74d88209b9ca53c35c61ecd14abcf5a2739d5999
prem3dev <ecstasy1508@gmail.com>
2025-06-09 09:28:19 +0900

    feat: StatType enum에 Straight, Flush 류들 고유의 StatType 추가

--- backend/src/main/java/com/example/rabbithell/common/effect/enums/StatType.java
+++ backend/src/main/java/com/example/rabbithell/common/effect/enums/StatType.java
@@ -3,7 +3,11 @@ package com.example.rabbithell.common.effect.enums;
 public enum StatType {
 	ATTACK_UP(StatCategory.BATTLE_STAT, DomainType.GAME_CHARACTER),
 	GOLD_RATE(StatCategory.REWARD_MODIFIER, DomainType.CLOVER),
-	DISCOUNT(StatCategory.META_BUFF, DomainType.CLOVER);
+	DISCOUNT(StatCategory.META_BUFF, DomainType.CLOVER),
+	STRAIGHT_STAT(StatCategory.BATTLE_STAT, DomainType.CLOVER),
+	FLUSH_STAT(StatCategory.REWARD_MODIFIER, DomainType.CLOVER),
+	STRAIGHT_FLUSH_STAT(StatCategory.META_BUFF, DomainType.CLOVER),
+	ROYAL_STRAIGHT_FLUSH_STAT(StatCategory.META_BUFF, DomainType.CLOVER);
 
 	private final StatCategory category;
 	private final DomainType domainType;
