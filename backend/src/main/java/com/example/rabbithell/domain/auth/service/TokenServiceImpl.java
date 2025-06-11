96f30cb6c0901d8e783bc2110bec0781502e87c8
giwon-wg <giwon8766@naver.com>
2025-06-11 15:44:27 +0900

    refactor: 코드리뷰내용 반영

---
+++ backend/src/main/java/com/example/rabbithell/domain/auth/service/TokenServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.rabbithell.domain.auth.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.rabbithell.domain.clover.entity.Clover;
+import com.example.rabbithell.domain.clover.repository.CloverRepository;
+import com.example.rabbithell.domain.clover.service.CloverServiceImpl;
+import com.example.rabbithell.domain.user.model.User;
+import com.example.rabbithell.domain.user.repository.UserRepository;
+import com.example.rabbithell.infrastructure.security.jwt.JwtUtil;
+import com.example.rabbithell.domain.auth.dto.response.TokenResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TokenServiceImpl implements TokenService {
+
+	private final JwtUtil jwtUtil;
+	private final UserRepository userRepository;
+	private final CloverServiceImpl cloverService;
+
+	@Override
+	@Transactional
+	public TokenResponse createFullToken(Long userId, String cloverName, Long kingdomId) {
+
+		User user = userRepository.findByIdOrElseThrow(userId);
+
+		Clover clover = cloverService.createClover(user, cloverName, userId, kingdomId);
+
+		String accessToken = jwtUtil.generateAccessToken(user.getId().toString(), "USER", clover.getId(), clover.getName());
+		String refreshToken = jwtUtil.generateRefreshToken(user.getId().toString());
+
+		return new TokenResponse(accessToken, refreshToken);
+	}
+}
--- backend/src/main/java/com/example/rabbithell/infrastructure/security/jwt/JwtProperties.java
+++ backend/src/main/java/com/example/rabbithell/infrastructure/security/jwt/JwtProperties.java
@@ -4,32 +4,28 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Component
 @ConfigurationProperties(prefix = "spring.jwt")
 @Getter
-@Setter
 public class JwtProperties {
+
     private String secret;
-    private Token token;
+    private final Token token = new Token();
 
     @Getter
-    @Setter
     public static class Token {
         private String header;
         private String prefix;
-        private Access access;
-        private Refresh refresh;
+        private final Access access = new Access();
+        private final Refresh refresh = new Refresh();
 
         @Getter
-        @Setter
         public static class Access {
             private long minute;
         }
 
         @Getter
-        @Setter
         public static class Refresh {
             private long minute;
         }
