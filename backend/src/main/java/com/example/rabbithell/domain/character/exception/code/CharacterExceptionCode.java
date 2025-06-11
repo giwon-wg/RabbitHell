991512077d636971ef517860fb746a8056dc1531
x0x22 <smallsol.fa@gmail.com>
2025-06-11 16:28:25 +0900

    Feat/skill (#126)
    * fix : 사용하지 않는 repository 삭제
    
    * feat : 최대 스탯 추가
    
    * feat : 캐릭터 소유자 검증 메서드 추가
    
    * refactor : max -> min 으로 변경
    
    * refactor : max -> min 으로 변경
    
    * feat : 스킬 학습 로직 구현
    
    * refactor : 테이블명 character -> game_character 변경 및 user 유니크 설정 제거
    
    * feat : 전직 기록 저장 및 전직 전적 확인 메서드 구현 및 스킬포인트 업데이트 메서드, 최소 스탯, 최소스탯 업데이트 메서드, 행운 업데이트 메서드, 스킬 포인트 소모 메서드 구현
    
    * refactor : currentSkill -> currentJobPoint 변경
    
    * feat : 스킬 확인용 메서드 구현
    
    * refactor : int -> Integer로 변경
    
    * refactor : minxStrength -> minStrength 오타 수정
    
    * refactor : characterId 제거
    
    * fix : NotBlank -> NotNull 변경
    
    * refactor : 불필요한 import 제거
    
    * refactor : 사용되지 않는 minStrength,minAgility,minIntelligence,minFocus,minLuck 제거
    
    * refactor : min~ 제거
    
    * feat : 스킬용 예외추가
    
    * feat : 스킬포인트 및 발동확률 추가 및 빌더 수정
    
    * feat : 커스텀 예외 처리
    
    * feat : 스킬 예외 추가
    
    * feat : 스키마 직업 example 변경
    
    * refactor : 스킬 학습 로직 SkillServiceImpl로 이동
    
    * feat : 스킬 학습 구현
    
    * feat : CharacterSkill 엔티티 필드 수정
    
    * feat : CharacterSkill 연관관계 설정
    
    * feat : CharacterSkill 부분 주석처리
    
    * feat : TODO 주석
    
    * refactor : import 주석처리
    
    * feat : 경험치 및 스킬포인트 직업별 숙련도 레벨 업데이트 메서드 추가
    
    * feat : 캐릭터 스킬 예외 코드 추가
    
    * refactor : 스킬 서비스에서 캐릭터 스킬 관련 로직 분리
    
    * feat : 캐릭터 스킬 학습, 장착, 해제, 조회 기능 구현
    
    * refactor : 사용하지 않는 UserSkillController 제거
    
    * feat : 스킬 타입(패시브,액티브), 스킬타겟(개인용,클로버용) 추가
    
    * refactor : 스탯 관련 메서드 추가
    
    * refactor : 캐릭터 생성시 luck 8 -> 100 으로 변경
    
    * feat : 전직시 스탯 생성 및 레벨 초기화 구현, 필요한 익셉션 추가
    
    * feat : 스킬 장착/해제 기능 구현 및 검증 로직 추가
    
    * feat : 스킬 장착 로직에 직업 일치 및 티어 검증 로직 추가
    
    * feat : 스킬 장착 시 슬롯 개수 제한

--- backend/src/main/java/com/example/rabbithell/domain/character/exception/code/CharacterExceptionCode.java
+++ backend/src/main/java/com/example/rabbithell/domain/character/exception/code/CharacterExceptionCode.java
@@ -12,7 +12,10 @@ public enum CharacterExceptionCode {
 	CHARACTER_NOT_FOUND(false, HttpStatus.NOT_FOUND, "해당 ID의 캐릭터를찾을 수 없습니다."),
 	ACCESS_DENIED(false, HttpStatus.FORBIDDEN, "본인의 캐릭터만 변경할 수 있습니다."),
 	LEVEL_IS_LOW(false, HttpStatus.BAD_REQUEST, "전직 최소 레벨은 50 이상입니다."),
-	SKILL_POINT_IS_LOW(false, HttpStatus.BAD_REQUEST, "숙련도가 부족하여 전직할 수 없습니다.");
+	SKILL_POINT_IS_LOW(false, HttpStatus.BAD_REQUEST, "숙련도가 부족하여 전직할 수 없습니다."),
+	MAIN_JOB_POINT_IS_LOW(false, HttpStatus.BAD_REQUEST, "직업 포인트가 부족하여 전직할 수 없습니다."),
+	SUB_JOB_POINT_IS_LOW(false, HttpStatus.BAD_REQUEST, "타 직업 포인트가 부족하여 전직할 수 없습니다.")
+	;
 
 	private final boolean success;
 	private final HttpStatus status;
