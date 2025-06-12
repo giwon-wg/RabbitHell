8b763ab66df5fc23f88568f9b7e2323773370720
prem3dev <ecstasy1508@gmail.com>
2025-06-12 18:04:45 +0900

    feat: BattleStatDto에서 PawCardBattleStatDto로 이름 변경

--- backend/src/main/java/com/example/rabbithell/common/effect/applier/BattleStatApplier.java
+++ backend/src/main/java/com/example/rabbithell/common/effect/applier/BattleStatApplier.java
@@ -1,10 +1,10 @@
 package com.example.rabbithell.common.effect.applier;
 
 import com.example.rabbithell.common.effect.enums.StatType;
-import com.example.rabbithell.domain.util.battleLogic.BattleStatDto;
+import com.example.rabbithell.domain.util.battleLogic.PawCardBattleStatDto;
 
 public interface BattleStatApplier {
 	StatType supports();
 
-	void apply(Integer ratioPercent, BattleStatDto battleStatDto);
+	void apply(Integer ratioPercent, PawCardBattleStatDto pawCardBattleStatDto);
 }
