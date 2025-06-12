fe6b79e9e0349a63cd33b39e5e6dda40748349bd
prem3dev <ecstasy1508@gmail.com>
2025-06-12 18:25:22 +0900

    feat: PawCardBattleStatDto 생성
    - BattleStat 전용 좀더 필드가 늘어날 예정

---
+++ backend/src/main/java/com/example/rabbithell/domain/util/battleLogic/PawCardBattleStatDto.java
@@ -0,0 +1,31 @@
+package com.example.rabbithell.domain.util.battleLogic;
+
+import lombok.Getter;
+
+@Getter
+public class PawCardBattleStatDto {
+
+	private Integer attackBonus = 0;
+	private Integer defenseBonus = 0;
+	private Integer speedBonus = 0;
+	private Integer attackPercent = 0;
+
+
+
+
+	public static PawCardBattleStatDto create () {
+		return new PawCardBattleStatDto();
+	}
+
+	public void applyAttackBonus(Integer value) {
+		if (value != null) this.attackBonus += value;
+	}
+
+	public void applyDefenseBonus(Integer value) {
+		if (value != null) this.defenseBonus += value;
+	}
+
+	public void applySpeedBonus(Integer value) {
+		if (value != null) this.speedBonus += value;
+	}
+}
