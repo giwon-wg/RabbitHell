71ddceaed9f5afc1fb8567db9827520054892a0b
prem3dev <ecstasy1508@gmail.com>
2025-06-12 18:16:25 +0900

    feat: DeckRedisService 생성

---
+++ backend/src/main/java/com/example/rabbithell/domain/deck/service/DeckRedisService.java
@@ -0,0 +1,33 @@
+package com.example.rabbithell.domain.deck.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.rabbithell.common.service.RedisService;
+import com.example.rabbithell.domain.deck.dto.DeckRedisDto;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DeckRedisService {
+
+	private final RedisService redisService;
+	private static final String DECK_KEY_PREFIX = "deck:";
+
+	public void saveDecks(Long cloverId, List<DeckRedisDto> deckRedisDtoList) {
+		String key = DECK_KEY_PREFIX + cloverId;
+		redisService.set(key, deckRedisDtoList);
+	}
+
+	public List<DeckRedisDto> getDecks(Long cloverId) {
+		String key = DECK_KEY_PREFIX + cloverId;
+		return redisService.get(key, new TypeReference<List<DeckRedisDto>>() {}); // 타입 안전화하려면 TypeReference 방식도 가능
+	}
+
+	public void deleteDecks(Long cloverId) {
+		redisService.delete(DECK_KEY_PREFIX + cloverId);
+	}
+}
