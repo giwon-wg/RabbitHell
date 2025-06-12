b2297c558f89a85e729a195a08040a600fc2b745
jiyun-im-dev <133330951+jiyun-im-dev@users.noreply.github.com>
2025-06-12 11:50:33 +0900

    Feat/back-office/item: 백오피스용 API 추가 등 (#128)
    * feat: 백오피스 - 아이템 집계 현황 추가
    
    * fix: 인벤토리 아이템 전체 조회 로직 수정 (슬롯 삭제)

--- backend/src/main/java/com/example/rabbithell/domain/inventory/controller/InventoryItemController.java
+++ backend/src/main/java/com/example/rabbithell/domain/inventory/controller/InventoryItemController.java
@@ -50,19 +50,18 @@ public class InventoryItemController {
 	}
 
 	@GetMapping
-	public ResponseEntity<CommonResponse<PageResponse<InventoryItemResponse>>> getAllInventoryItemsBySlot(
+	public ResponseEntity<CommonResponse<PageResponse<InventoryItemResponse>>> getAllInventoryItems(
 		@AuthenticationPrincipal AuthUser authUser,
 		@RequestParam(defaultValue = "0") int page,
-		@RequestParam(defaultValue = "10") int size,
-		@RequestParam(required = false) Slot slot
+		@RequestParam(defaultValue = "10") int size
 	) {
 		Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
 
 		return ResponseEntity.ok(CommonResponse.of(
 			true,
 			HttpStatus.OK.value(),
-			"(슬롯 별) 인벤토리 아이템 전체 조회 성공",
-			inventoryItemService.getAllInventoryItemsFilterBySlot(authUser.getUserId(), slot, pageable)
+			"인벤토리 아이템 전체 조회 성공",
+			inventoryItemService.getAllInventoryItems(authUser.getUserId(), pageable)
 		));
 	}
 
